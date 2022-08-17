a4983f5e10cf8e551f70286267304af832429238
Luiz Eduardo Campos da Silva <luizcampos331@gmail.com>
2022-08-16 21:32:33 -0300

    Fix - schema

--- src/infra/http/functions/translate/index.ts
+++ src/infra/http/functions/translate/index.ts
@@ -8,7 +8,7 @@ export default {
       http: {
         method: 'post',
         path: 'handler',
-        require: {
+        request: {
           schemas: {
             'appication/json': schema
           }
